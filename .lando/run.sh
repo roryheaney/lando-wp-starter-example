6109b664e535c4471ba87fd6fe58075ef7c24acb
Rory <rory@fancysquares.com>
2025-02-21 14:44:35 -0800

    Add phpmyadmin and use the default lando url instead of a local one

--- .lando/run.sh
+++ .lando/run.sh
@@ -2,6 +2,10 @@ if [ ! -d "./wp-admin" ]; then
 	wp core download
 fi
 
+# if ! $(wp core is-installed); then
+# 	wp core install --url="http://wp-starter.local" --title="WP Starter" --admin_user="admin" --admin_password="password" --admin_email="admin@wp-starter.local" --skip-email
+# fi
+
 if ! $(wp core is-installed); then
-	wp core install --url="http://wp-starter.local" --title="WP Starter" --admin_user="admin" --admin_password="password" --admin_email="admin@wp-starter.local" --skip-email
+	wp core install --url="http://$LANDO_APP_NAME.lndo.site" --title="WP Starter" --admin_user="admin" --admin_password="password" --admin_email="admin@$LANDO_APP_NAME.lndo.site" --skip-email
 fi
